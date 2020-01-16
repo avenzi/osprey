b9a2acf649ffa82858c7c47c99e8dab0114ae67d
Rainier Devolder <devolde2@msu.edu>
2020-01-15 20:31:46 -0500

    add working test code for executing sql against db

--- site/controllers/home.py
+++ site/controllers/home.py
@@ -1,6 +1,9 @@
 from flask import render_template
-from main import app
+from main import app, mysql
 
 @app.route("/")
 def greeting():
+    # Debug - works and creates a table if everything is configured correctly
+    #database_cursor = mysql.connection.cursor()
+    #database_cursor.execute('''CREATE TABLE test (id INTEGER, name VARCHAR(20))''')
     return render_template('home/start.html', var=None)
