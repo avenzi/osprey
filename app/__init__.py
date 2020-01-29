2a3f208dbc1c0577afcf1051bcdcf7f663cd93d1
root <root@vps277289.vps.ovh.ca>
2020-01-28 21:46:30 -0500

    Deleting unnecessary POC files and adding skeleton for clean start point

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
-import os
-from flask import Flask, render_template
-#from flask_socketio import SocketIO
+from flask import Flask
 from flask_mysqldb import MySQL
 
-app = Flask(__name__, template_folder='templates')
-
-#socketio = SocketIO(app)
+app = Flask(__name__)
 
 #------------------------------------------
 # Configure the database connection
@@ -15,8 +11,7 @@ app.config['MYSQL_USER'] = 'CapstoneMySQLUser'
 app.config['MYSQL_PASSWORD'] = 'CapstoneMySQLUserDbPw'
 app.config['MYSQL_DB'] = 'CapstoneData'
 
-# Establish the connection to the database
+# Establish connection to the database
 mysql = MySQL(app)
 
-# Runs __init__.py in controllers module, which establishes all the routes
-from controllers import *
+from app.controllers import routes
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd site
 gunicorn --bind 0.0.0.0:5000 capstone:app
