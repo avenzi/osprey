7b54641cc4405be458aeef5d8d92f73453dcc052
root <root@vps277289.vps.ovh.ca>
2020-01-29 00:11:59 -0500

    adding more skeleton code to work off of

---
+++ app/controllers/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    # DataRequired() validator method checks that fields are not submitted empty
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,6 +1,34 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.controllers.forms import LoginForm
 
+@app.route('/')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    #-----------------
+    # The validate_on_submit() method does all form processing work and returns true when a form
+    # is submitted and the browser sends a POST request indicating data is ready to be processed
+    #---------------
+    if form.validate_on_submit():
+        # A template in the application is used to render flashed messages that Flask stores
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/livefeed')
+def livefeed():
+    return render_template('livefeed.html')
+
+@app.route('/archives')
+def archives():
+    return render_template('archives.html')
