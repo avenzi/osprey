f05ceb40b74e7af6d53f4054c2e4dabc7b2f6648
Rainier Devolder <devolde2@msu.edu>
2020-04-20 05:42:23 -0400

    remove unused function

--- app/static/JS/menu.js
+++ app/static/JS/menu.js
@@ -7,13 +7,3 @@ function closeNav()
 {
     document.getElementById("theSidebar").style.width = "0";
 }
-
-(function() {
-    /*
-    document.getElementById("defaultChecked1").addEventListener("click", function(){
-        var vis = document.getElementById("defaultChecked1").checked;
-        console.log(vis);
-        document.getElementById("video-archives").style.visibility = (vis === false) ? "hidden" : "visible";
-    });
-    */
- })();
