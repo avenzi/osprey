85dbc93b21ea6383f00375d2d25d324548b0ce04
Rainier Devolder <devolde2@msu.edu>
2020-04-20 05:58:09 -0400

    slight audio listener tweaks and remove debug

--- data-ingestion/audio_listener.py
+++ data-ingestion/audio_listener.py
@@ -17,7 +17,6 @@ class AudioListener(Listener):
         if AudioListener.has_init == False:
             AudioListener.has_init = True
             self.init()
-            print("RAN INIT")
         
     def get_session_data(self):
         sql = """SELECT * FROM Session WHERE StartDate = (SELECT MAX(StartDate) FROM Session)"""
@@ -29,7 +28,6 @@ class AudioListener(Listener):
         sql = """SELECT id FROM SessionSensor WHERE SessionId = %s AND IP = INET_ATON(%s) AND SensorType = 'Microphone';"""
         self.cursor.execute(sql, (AudioListener.session_id, self.client_address[0]))
         AudioListener.sensor_id = self.cursor.fetchone()['id']
-
         self.ensure_sensor_data()
     
     def init(self):
@@ -42,8 +40,8 @@ class AudioListener(Listener):
         AudioListener.data_directories = []
 
         AudioListener.frames_metadata = {}
-        AudioListener.frames_per_database_record = 3
-        AudioListener.frames_per_directory = 3
+        AudioListener.frames_per_database_record = 2
+        AudioListener.frames_per_directory = 10
         AudioListener.directory_number = {}
         AudioListener.current_record_frames_total = {}
         AudioListener.current_directory_frames_total = {}
@@ -84,15 +82,12 @@ class AudioListener(Listener):
         first_frame_number, last_frame_number, frames_metadata):
         
         compacted_json = json.dumps(frames_metadata, separators=(',', ':'), sort_keys=True)
-        print(compacted_json)
 
         sql = """INSERT INTO `AudioSegments` (`FirstSegmentTimestamp`, `LastSegmentTimestamp`, `FirstSegmentNumber`, `LastSegmentNumber`, `SessionId`, `SensorId`, `SegmentsMetadata`)
             VALUES (FROM_UNIXTIME(%s * 0.001), FROM_UNIXTIME(%s * 0.001), %s, %s, %s, %s, %s)"""
         
         self.cursor.execute(sql, (first_frame_timestamp, last_frame_timestamp, first_frame_number, last_frame_number, session_id, sensor_id, compacted_json))
         self.db_connection.commit()
-
-        print("Committed record to database")
     
     def ensure_sensor_data(self):
         if not self.sensor_id in AudioListener.current_directory_frames_total:
