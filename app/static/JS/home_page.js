4c5db6080f1d23dcda4d08eeaf8487ce6f9f3860
Rainier Devolder <devolde2@msu.edu>
2020-04-20 05:52:06 -0400

    remove old home page modal

--- app/static/JS/home_page.js
+++ app/static/JS/home_page.js
@@ -1,31 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Create New Session button handler
-    document.getElementById("new-session").addEventListener("click", function() {
-        // Display the overlays
-        var overlay = document.getElementById('overlay');
-        overlay.style.display = "unset";
-    });
-
-    document.getElementById("overlay").addEventListener("click", function() {
-        // Hide the overlays
-        var overlay = document.getElementById('overlay');
-        overlay.style.display = "none";
-    });
-
-    document.getElementById("overlay").addEventListener("click", function() {
-        // Hide the overlays
-        var overlay = document.getElementById('overlay');
-        overlay.style.display = "none";
-    });
-
-    document.getElementById("new-session-modal").addEventListener("click", function(event) {
-        event.stopPropagation();
-    });
-
-});
-
 function delete_session(session_id) {
-    // TODO: confirm delete modal / pop-up
     $.post(`delete_session/${session_id}`, {}, function(result) {
         $(`#entry${session_id}`).remove();
     });
