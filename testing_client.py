310aac8f05f2ceffc1edd44bdf11e003e9e4fb71
Aven <zitzelbe@msu.edu>
2020-05-25 15:19:08 -0400

    Client-side connection testing program

--- testing_client.py
+++ testing_client.py
@@ -1,16 +1,17 @@
 import socket
 
 host = "3.136.140.191"
-port = 8000
+port = 80
+enc = "utf-8"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
     print("Connected to {}:{}".format(host, port))
     
-    test = b'THIS IS A TEST STRING'
+    test = b'THIS STRING SENT FROM CLIENT'
     s.sendall(test)
-    print("Sent: ", test)
+    print("Sent: \n", test.decode(enc))
     
     data = s.recv(1024)
 
-print("Received: ", repr(data))
+print("Received: \n", data.decode(enc))
