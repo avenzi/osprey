911a497f17a33149f990f87a3ab87ed9268ec822
Aven Zitzelberger <zitzelbe@msu.edu>
2020-06-11 20:07:46 -0400

    Receiving code for client

--- testing/live_video_stream/ingestion.py
+++ testing/live_video_stream/ingestion.py
@@ -5,10 +5,4 @@ ip = '35.20.136.146'  # ip of raspberry pi
 port = 8000           # TCP port 8000
 
 handler = ServerHandler(ip, port)
-handler.connect()
-
-try:
-    while True:
-        handler.read()  # read from TCP stream
-finally:
-    handler.close()  # close connection
+handler.stream()
--- testing/live_video_stream/ingestion_lib.py
+++ testing/live_video_stream/ingestion_lib.py
@@ -33,24 +33,45 @@ class ServerHandler:
         self.header = None    # header
         self.content = None   # message content
 
-        self.num = 0   # number of images received
+        self.number_received = 0       # number of images received
+        self.number_sent = 0           # number of images sent from the pi (sent in header)
+
+    def stream(self):
+        """
+        Create and connect to socket,
+        then read from the TCP continually,
+        disconnecting on error.
+        """
+        self.connect()
+        try:
+            while True:
+                self.read()
+        finally:
+            self.close()
+            print("Frames Received: {}/{}".format(self.number_received, self.number_sent))
 
     def connect(self):
         """ Create and connect to socket via given address. """
         try:   # create socket object
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
-            print("Socket Created")
+            print("Socket Created...")
         except Exception as e:
             raise Exception("Failed to create socket: \n{}".format(e))
 
         try:  # connect socket to given address
             self.socket.connect((self.ip, self.port))
-            print("Socket Connected")
+            print("Socket Connected...")
         except Exception as e:
             raise Exception("Failed to connect to socket on {}:{} \n{}".format(self.ip, self.port, e))
 
+        # Send custom request method CLIENTSTREAM to let the server know that this is the ingestion client.
+        # The rest of the data sent is to just to fulfill the request syntax.
+        # The two newlines add a blank line to the request, denoting that its a complete packet
+        self.socket.sendall(b'CLIENTSTREAM /index.html HTTP/1.1 \r\n\n')
+        print('Sent Stream Request...')
+
     def read(self):
-        """ read from the TCP stream """
+        """ read from the TCP stream one time """
         try:
             data = self.socket.recv(4096)  # receive data from pi
         except BlockingIOError:  # Temporarily unavailable (errno EWOULDBLOCK)
@@ -59,7 +80,7 @@ class ServerHandler:
             if data:  # if received data
                 self.in_buffer += data  # append to in-buffer
             else:
-                raise RuntimeError('Peer closed.')
+                raise Exception('Stream Disconnected')
 
         if self.proto is None:      # haven't gotten proto yet
             self.parse_proto()
@@ -69,8 +90,8 @@ class ServerHandler:
             self.parse_content()
 
     def parse_proto(self):
-        """ Parse the proto-header from the start of the buffer """
-        proto_len = 2      # length of proto-header
+        """ Parse the proto-header from the start of the buffer, stores in self.proto """
+        proto_len = 2      # length of proto-header in bytes
 
         # Complete proto not yet received
         if len(self.in_buffer) < proto_len:
@@ -81,7 +102,7 @@ class ServerHandler:
         # print("Got proto:", self.proto)
 
     def parse_header(self):
-        """ Parse the header from the start of the buffer """
+        """ Parse the header from the start of the buffer, stores in self.header """
         header_len = self.proto
 
         # Complete header not yet received
@@ -93,7 +114,7 @@ class ServerHandler:
         # print("Got header:", self.header)
 
         # validate header
-        for info in ("byteorder", "length", "type", "encoding"):
+        for info in ('length', 'number'):
             if info not in self.header:
                 raise ValueError('Missing required header "{}"'.format(info))
 
@@ -108,26 +129,15 @@ class ServerHandler:
         data = self.in_buffer[:content_len]  # parse content
         self.in_buffer = self.in_buffer[content_len:]  # move down buffer
 
-        # handle content
-        if self.header['type'] == 'text/json':
-            encoding = self.header["encoding"]
-            self.content = decode_json(data, encoding)
-            # print("Received text response from [{}:{}]: \n{}",  self.ip, self.port, repr(self.content))
-            # self.process_json_content()
+        # Decode image data
+        stream = io.BytesIO(data)  # convert received data into bytesIO object for PIL
+        img = Image.open(stream)  # open object as JPEG
+        # print("Received image. Length: {}, Size: {}".format(len(data), img.size))
+        # img.save("./images/img_{}.png".format(strftime('%Y-%m-%d_%H-%M-%S')))  # save image
 
-        elif self.header["type"] == 'image/jpeg':
-            stream = io.BytesIO(data)  # convert received data into bytesIO object for PIL
-            img = Image.open(stream)  # open object as JPEG
-            # print("Received image. Length: {}, Size: {}".format(len(data), img.size))
-            # img.save("./images/img_{}.png".format(strftime('%Y-%m-%d_%H-%M-%S')))  # save image
-            self.num += 1  # received another image
-
-        else:  # Binary or unknown content-type
-            self.content = data
-            print('Received {} response from [{}:{}}', self.header["type"], self.ip, self.port)
-            # self.process_binary_content()
-
-        self.reset()
+        self.number_received += 1  # received another image
+        self.number_sent = self.header['number']  # update number sent from pi
+        self.reset()      # reset class attributes to prepare for another image
 
     def reset(self):
         """ Reset all message variables, ready to receive another """
@@ -139,4 +149,3 @@ class ServerHandler:
         """ Disconnect """
         self.socket.close()
         print("Disconnected")
-        print("Frames received:", self.num)
