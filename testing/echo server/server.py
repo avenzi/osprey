407cab869425139fb0a35305e74b3d4c426c34aa
Aven Zitzelberger <zitzelbe@msu.edu>
2020-06-30 15:03:33 -0400

    invesigating weird problem where the client can only receive data once, but can keep sending

--- testing/echo server/server.py
+++ testing/echo server/server.py
@@ -1,31 +1,36 @@
-import socket
+import socket, time
 
-host = ''           # allow any connection
-port = 5000
-enc = "utf-8"       # byte encoding
+host = ''  # allow any connection
+port = 5001  # TCP port 80
+enc = "utf-8"  # byte encoding
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Binding to {}:{}".format(host, port))
     s.bind((host, port))
-    
+
     print("Listening")
     s.listen()
-    
+
     conn, addr = s.accept()
     print("Accepted")
+    conn.setblocking(False)
 
     with conn:
         print('Connected by', addr)
-        while True:  # loop while data is being received
-            data = conn.recv(1024)
-            if not data:
-                break
+        i = 0
+        while True:
+            i += 1
+            try:
+                conn.sendall("THIS IS A TEST {}".format(i).encode(enc))
+                print("sent")
+                time.sleep(0.5)
+            except BlockingIOError:
+                pass
 
-            print("Received: \n", data.decode(enc))
+            try:
+                data = conn.recv(1024)
+                print("got: {}".format(data))
+            except BlockingIOError:
+                pass
 
-            # add message to send back
-            data = data.decode(enc) + "\n THIS LINE WAS SENT FROM THE SERVER"
-            data = data.encode(enc)  # encode back into byte object
 
-            conn.sendall(data)
-            print("Sent: \n", data.decode(enc))
