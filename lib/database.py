178e66f81fa8e62f3b609845df5ecd10ba20518e
Aven Zitzelberger <zitzelbe@msu.edu>
2021-06-28 18:49:15 -0400

    update

--- lib/database.py
+++ lib/database.py
@@ -219,9 +219,7 @@ class Database:
                     try:
                         output[output_keys[i]].append(float(d[keys[i]]))  # convert to float and append
                     except Exception as e:
-                        print(output)
-                        print(output_keys[i])
-                        print(d[keys[i]])
+                        print(d)
                         raise e
         else:
             for data in data_list:
