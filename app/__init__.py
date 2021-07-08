b844b52e1a200fbe2968a853eb449e49d72557d0
Aven Zitzelberger <zitzelbe@msu.edu>
2021-07-08 18:45:22 -0400

    update

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     # initialize database connection
     app.database = Database('3.131.117.61', 5001, 'thisisthepasswordtotheredisserver')
     app.database.init()
-    app.datanase.set_ready(True)  # ready to receive data
+    app.database.set_ready(True)  # ready to receive data
 
     # add basic favicon
     @app.route('/favicon.ico')
