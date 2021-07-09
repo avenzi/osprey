a57f648b032b748ee1e33e9a828c7b892efcc34d
Aven Zitzelberger <zitzelbe@msu.edu>
2021-07-09 15:43:20 -0400

    update

--- lib/database.py
+++ lib/database.py
@@ -542,6 +542,7 @@ class Database:
     def read_all_groups(self):
         """ Gets a list of dictionaries containing name and ID info for all connected streams """
         info = []
+        print("GETTING ALL INFO")
         for key in self.redis.execute_command('keys group:*'):
             info.append(self.redis.hgetall(key))
         return info
