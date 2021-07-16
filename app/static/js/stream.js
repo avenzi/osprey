e1a2867aae60c1b70e6ab4051a9515277a8ed2e8
Aven Zitzelberger <zitzelbe@msu.edu>
2021-07-16 12:53:12 -0400

    update

--- app/static/js/stream.js
+++ app/static/js/stream.js
@@ -10,6 +10,48 @@ function error(msg) {
     $('.logs p').prepend(`> <span style="color:red;font-weight:bold;">${msg}</span><br>`);
 }
 
+function get_plot() {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200)
+            embed_plot(req.responseText);  // once data is received, embed the plot
+    }
+    url = window.location.pathname
+    queries = window.location.search
+    req.open("GET", url+'/plot_layout'+queries, true);
+    req.send(null);   // send GET request for plot JSON
+    console.log('sent request: '+url+'/plot'+queries)
+}
+
+function embed_plot(text) {
+    json = JSON.parse(text)
+    window.Bokeh.embed.embed_item(json, 'streamplot');  // embed received JSON into div with id='streamplot'
+    console.log("Embedded initial plot")
+}
+
+function check_bokeh() {
+    if (window.Bokeh !== undefined) {
+        get_plot();
+    } else {
+        attempts = 0;
+    timer = setInterval(attempt_load, 10, attempts)
+    }
+}
+
+function attempt_load(attempts) {
+    console.log("Failed to load - trying again ("+attempts+")")
+    if (window.Bokeh !== undefined) {
+        clearInterval(timer);
+        get_plot();
+    } else {
+        attempts++;
+        if (attempts > 100) {
+            clearInterval(timer);
+            console.log("Bokeh: ERROR: Unable to run BokehJS code because BokehJS library is missing");
+        }
+    }
+}
+
 $(document).ready(function() {
     var namespace = '/browser';  // namespace for talking with server
     var socket = io(namespace);
@@ -30,7 +72,11 @@ $(document).ready(function() {
         //error(msg)
     });
 
-    // request update every second
+    // set up bokeh
+    if (document.readyState != "loading") Bokeh.safely(check_bokeh);
+    else document.addEventListener("DOMContentLoaded", Bokeh.safely(check_bokeh));
+
+    // request time update every second
     setInterval(function() {
         socket.emit('stream_time');
     }, 1000);
