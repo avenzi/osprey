9f5e0672a916ec8d8ae076c0e967b18a19cde372
Aven Zitzelberger <zitzelbe@msu.edu>
2021-08-10 13:40:25 -0400

    update

--- app/static/js/bokeh_stream.js
+++ app/static/js/bokeh_stream.js
@@ -18,6 +18,7 @@ function get_id() {
 
 function get_plot() {
     // todo: Use SocketIO instead just for consistency's sake?
+    // sends a request to the server for the Bokeh plot page
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if (req.readyState == 4 && req.status == 200)
@@ -31,12 +32,14 @@ function get_plot() {
 }
 
 function embed_plot(text) {
+    // Use bokeh to embed the plot into the page
     json = JSON.parse(text)
     window.Bokeh.embed.embed_item(json, 'streamplot');  // embed received JSON into div with id='streamplot'
     console.log("Embedded initial plot")
 }
 
 function check_bokeh() {
+    // verify that bokeh is functioning in the page
     if (window.Bokeh !== undefined) {
         get_plot();
     } else {
@@ -46,6 +49,7 @@ function check_bokeh() {
 }
 
 function attempt_load(attempts) {
+    // attempt to load the bokeh plot
     console.log("Failed to load - trying again ("+attempts+")")
     if (window.Bokeh !== undefined) {
         clearInterval(timer);
@@ -57,6 +61,42 @@ function attempt_load(attempts) {
             console.log("Bokeh: ERROR: Unable to run BokehJS code because BokehJS library is missing");
         }
     }
+}
+
+function add_method(current_value) {
+    // add a new dropdown menu to the top of the page with a given list of methods
+    // to select another custom function to add to the pipeline.
+    // If current_value is given, set that as the selected option.
+    select = $(`<select class="function_select" onchange="function_select_change(this);">\
+                   <option value="">--Select a function--</option>\
+                </select>
+    `).appendTo("div.custom_functions div.menus");
+
+    // set current value, if given
+    if (current_value != undefined) {
+        select.value = current_value
+    }
+
+    // add an option for each method in the new select menu
+    for (func of functions) {
+        select.append(`<option value="${func}">${func}</option>`)
+    }
+
+    // assign an order number to the select menu
+    select.data('order', pipeline.length));
+
+    console.log("NEW MENU: "+order+"  "+select.value);
+}
+
+function function_select_change(select) {
+    // called when a method is selected in a dropdown menu.
+    // adds selected value to pipeline in the right spot
+    order = select.data('order');
+    name = select.value;
+    pipeline[order] = name
+    console.log(`order: ${order}, val: ${value}`)
+    console.log(pipeline)
+    socket.emit('update_pipeline', {group: id, pipeline: pipeline})
 }
 
 $(document).ready(function() {
@@ -93,4 +133,24 @@ $(document).ready(function() {
     socket.on('stream_time', function(data) {
         $("div.stream_time").html(data);
     });
+
+    // Custom function interface
+    var functions;  // list of function names available
+    var pipeline;   // list of function names in the current pipeline
+    var analyzer_id;  // ID of analyzer class to u
+
+    // request a list of the available function names and
+    //  a list of the currently selected functions for this page
+    socket.emit('custom_functions', id);
+    socket.on('custom_functions', function(data) {  // receive this info
+        functions = data.functions;
+        console.log("new funcs: " + functions)
+        $("div.custom_functions div.menus").empty()  // clear current selections
+        for (selected of data.selected) {  // for each selected function
+            add_method(selected)
+        }
+    });
+
+    // add functionality to the + button, adds another dropdown menu
+    $("div.custom_functions button.add").on('click', add_method)
 });
