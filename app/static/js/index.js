1ccce4b7fc08fc484aacfabd617946c72f9768f1
Aven Zitzelberger <zitzelbe@msu.edu>
2021-08-13 15:07:19 -0400

    update

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -181,6 +181,8 @@ $(document).ready(function() {
                 // send file contents through SocketIO
                 // 'file' is the name attribute of the form input tag
                 console.log(formData.has())
+                console.log(formData.keys())
+                console.log(formData.values())
                 socket.emit('upload', {name: formData.get('filename'), content: formData.get('file')})
                 $(this).dialog("close");
             },
