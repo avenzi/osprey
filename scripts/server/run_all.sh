be83ed1c080990ed728a61f5c7f790b8ff975103
Aven Zitzelberger <zitzelbe@msu.edu>
2021-08-16 22:13:32 -0400

    update

--- scripts/server/run_all.sh
+++ scripts/server/run_all.sh
@@ -3,15 +3,13 @@
 # ensure the CWD is the directory of this script
 script_dir="$(dirname $(realpath $0))"
 cd $script_dir
-# navigate to top level dir
-cd ../../
 
 # update from git
 #echo "Updating From Git..."
 #git pull
 
 # kill any remaining processed
-bash scripts/server/quit.sh
+bash quit.sh
 
 bash run_stream.sh
 
