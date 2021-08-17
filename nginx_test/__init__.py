bc6b74060f876d0fbb909b4892b6e0eeff9dcec2
Aven Zitzelberger <zitzelbe@msu.edu>
2021-08-16 21:54:44 -0400

    update

--- nginx_test/__init__.py
+++ nginx_test/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send_from_directory
 
 
 def create_app():
     """ Application factory to create the app and be passed to workers """
     app = Flask(__name__)
+
+    @app.route('/index', methods=['GET', 'POST'])
+    def flask_page():
+        return send_from_directory('index.html')
 
     @app.route('/flask_page', methods=['GET', 'POST'])
     def flask_page():
