5c42770e8cf0772f1db1a50c1bbf4e6a9dad8f03
Aven Zitzelberger <zitzelbe@msu.edu>
2021-09-01 16:01:36 -0400

    update

--- pyaudiotest.py
+++ pyaudiotest.py
@@ -2,6 +2,6 @@ import sounddevice as sd
 
 sd.default.samplerate = 44100
 sd.default.channels = 1
-myrecording = sd.rec(int(5 * sd.default.samplerate), samplerate=sd.default.samplerate, channels=2)
+myrecording = sd.rec(int(5 * sd.default.samplerate), samplerate=sd.default.samplerate, channels=1)
 sd.wait()
 print(len(myrecording))
