9eaec651910a58305f9aefaa1b626acf71652bb7
Aven Zitzelberger <zitzelbe@msu.edu>
2021-09-01 17:15:35 -0400

    update

--- pyaudiotest.py
+++ pyaudiotest.py
@@ -1,7 +1,9 @@
 import sounddevice as sd
 
-sd.default.samplerate = 44100
-sd.default.channels = 1
-myrecording = sd.rec(int(5 * sd.default.samplerate), samplerate=sd.default.samplerate, channels=1)
-sd.wait()
-print(len(myrecording))
+stream = sd.Stream(channels=1)
+
+while True:
+    frames = stream.read_available
+    print(frames)
+    stream.read(frames)
+    sd.sleep(1)
