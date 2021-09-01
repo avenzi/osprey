fadeee923d105aded5ae3ace67735531a4285960
Aven Zitzelberger <zitzelbe@msu.edu>
2021-09-01 18:27:57 -0400

    update

--- pyaudiotest.py
+++ pyaudiotest.py
@@ -1,16 +1,17 @@
 import sounddevice as sd
 from time import sleep
 
-'''
+
 stream = sd.InputStream(channels=1)
 stream.start()
 while True:
     frames = stream.read_available
     print(frames)
     out = stream.read(frames)
-    sleep(1)
-    '''
+    sleep(0.1)
 
+'''
+# working
 fs=44100
 duration=5
 print("recording...............")
@@ -18,3 +19,4 @@ print("recording...............")
 recording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
 sd.wait()
 print(recording, sum(recording))
+'''
