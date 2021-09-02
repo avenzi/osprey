13cb1a4908fc54324538563bab9e4f3bd1ef7bad
Aven Zitzelberger <zitzelbe@msu.edu>
2021-09-02 16:51:09 -0400

    update

--- lib/raspi/streamers.py
+++ lib/raspi/streamers.py
@@ -194,7 +194,9 @@ class AudioStreamer(Streamer):
     def __init__(self, *args):
         super().__init__(*args)
 
-        self.audio_buffer = BytesOutput2()  # buffer to hold images from the Picam
+        #self.audio_buffer = BytesOutput2()  # buffer to hold images from the Picam
+        from io import BytesIO
+        self.audio_buffer = BytesIO()
         self.sample_rate = 44100
 
         import sounddevice as sd
