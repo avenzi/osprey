4e7a1cc1c8fed833003470d691d98f0079ce9d1d
Aven Zitzelberger <zitzelbe@msu.edu>
2021-09-08 09:22:13 -0400

    update

--- raspi/run_pi.py
+++ raspi/run_pi.py
@@ -5,17 +5,14 @@ from lib.raspi.streamers import *
 t1 = TestStreamer("Random 1", "Test Group 1")
 t2 = TestStreamer("Random 2", "Test Group 1")
 
-# Synth EEG Stream #1
 synth1eeg = SynthEEGStreamer('Raw', 'Synth EEG 1')
 
 video = VideoStreamer('Video', 'Video 1')
 
-audio = AudioStreamer('Audio', 'Video 1')
-
 sense = SenseStreamer('Raw', 'Sense Hat 1')
 
 # Pass all workers to client
-workers = [t1, t2, synth1eeg, video, sense, audio]
+workers = [t1, t2, synth1eeg, video, sense]
 
 client = Client(
     workers=workers, name='Raspi #1', debug=1,
