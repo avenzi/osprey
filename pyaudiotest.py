f7b8a611b8d5abe288d79fd65763928962e2681e
Aven Zitzelberger <zitzelbe@msu.edu>
2021-09-09 12:55:12 -0400

    update

--- pyaudiotest.py
+++ pyaudiotest.py
@@ -4,24 +4,23 @@ import soundfile as sf
 from lib.raspi.pi_lib import BytesOutput2
 from io import BytesIO
 from time import sleep
+from threading import Thread
 
 in_buf = BytesOutput2()
 out_buf = BytesIO()
 samplerate = 44100
 channels = 1
+signal = False
 
 file = sf.SoundFile(in_buf, mode='w', samplerate=samplerate, channels=channels, format='WAV')
 
-
+# write to buffer
 def callback(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
     file.write(indata)
 
 
 stream = sd.InputStream(samplerate=samplerate, channels=channels, callback=callback)
-
-stream.start()
-print('started')
 
 ffmpeg_process = (
     ffmpeg
@@ -30,38 +29,58 @@ ffmpeg_process = (
     .run_async(pipe_stdin=True, pipe_stdout=True)
 )
 
-for i in range(10):
-    print('start of loop')
-    in_data = in_buf.read()
-    print('in buf data: ', len(in_data))
-    if not in_data:
-        print('no data read from in_buf')
+# write to ffmpeg
+def write():
+    while not signal:
+        in_data = in_buf.read()
+        print('in buf data: ', len(in_data))
+        if not in_data:
+            print('no data read from in_buf')
+            sleep(1)
+            continue
+
+        written_data = ffmpeg_process.stdin.write(in_data)
+        print('written data:', len(in_data))
+        if not written_data:
+            print('no data written')
+            sleep(1)
+            continue
+
         sleep(1)
-        continue
+    print('ended write thread')
+Thread(target=write).start()
 
-    written_data = ffmpeg_process.stdin.write(in_data)
-    print('written data:', len(in_data))
-    if not written_data:
-        print('no data written')
+
+# read from ffmpeg
+def read():
+    while not signal:
+        print('gonna read')
+        out_data = ffmpeg_process.stdout.read()
+        print('ffmpeg out data:', len(out_data))
+        if not out_data:
+            print('no data read back from ffmpeg')
+            sleep(1)
+            continue
         sleep(1)
-        continue
+    print('ended read thread')
+Thread(target=read).start()
 
-    print('gonna read')
-    out_data = ffmpeg_process.stdout.read()
-    print('ffmpeg out data:', len(out_data))
-    if not out_data:
-        print('no data read back from ffmpeg')
-        sleep(1)
-        continue
-    sleep(1)
 
-print('total in_buf:', len(in_buf.getvalue()))
-print('total out_buf:', len(out_buf.getvalue()))
+stream.start()
+print('started')
 
+sleep(10)
 
 stream.stop()
 print('stopped')
 
 ffmpeg_process.wait()
 print('finished waiting')
+
+signal = True
+print('set signal')
+
+print('total in_buf:', len(in_buf.getvalue()))
+print('total out_buf:', len(out_buf.getvalue()))
+
 
