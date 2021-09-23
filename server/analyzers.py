1043a9b48e61b3c8a4a6665c12d4aab0d3512ab6
Aven Zitzelberger <zitzelbe@msu.edu>
2021-09-23 11:50:36 -0400

    update

--- server/analyzers.py
+++ server/analyzers.py
@@ -97,12 +97,14 @@ class AudioEncoder(Analyzer):
             data = data_dict['data']
             data = np.expand_dims(np.array(data, dtype='float32'), axis=1)
             self.ffmpeg_process.stdin.write(data)
+            print('written to ffmpeg', len(data))
             sleep(0.01)
         else:
             sleep(0.2)
 
     def read_from_ffmpeg(self):
         """ Meant to be run on a seaprate thread. Write encoded audio to the database """
+        print('gonna read from ffmpeg')
         encoded_audio = self.ffmpeg_process.stdout.read(256)
         if not encoded_audio:
             sleep(1)
